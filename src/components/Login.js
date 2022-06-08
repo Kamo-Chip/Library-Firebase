e102af359c8761d3f547062c34d47a7ce79edafe
Kamogelo Khumalo <kamokhumalo04@gmail.com>
2022-06-08 21:49:03 +0200

    Created login form

---
+++ src/components/Login.js
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import { auth } from "../firebase";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
+
+const Login = () => {
+
+    const [ user, setUser ] = useState({
+        email: "",
+        password: "",
+        loading: false,
+        error: null,
+    });
+
+    const navigate = useNavigate();
+
+    const handleChange = (e) => {
+        e.preventDefault();
+        setUser({...user, [e.target.name]: e.target.value});
+    }
+
+    const signIn = async (e) => {
+        e.preventDefault();
+
+        await signInWithEmailAndPassword(auth, user.email, user.password)
+        .catch(err => setUser({...user, error: err.message}));
+
+        navigate("/library");
+    }
+    
+    return (
+        <form onSubmit={signIn} className="login">
+            <section>
+                <label>Email:</label>
+                <input type="email" name="email" onChange={handleChange} value={user.email}/>
+            </section>
+            <section>
+                <label htmlFor="password">Password:</label>
+                <input type="password" name="password" onChange={handleChange} value={user.password}/>
+            </section>
+            <button>Login</button>
+            { user.error ? <p>{user.error}</p> : null}
+        </form>
+    )
+}
+
+export default Login;
