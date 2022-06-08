fcd49bc49db6b4cdd01540e9b64d00e7ef1eed3e
Kamogelo Khumalo <kamokhumalo04@gmail.com>
2022-06-08 21:49:23 +0200

    Created registration form

---
+++ src/components/Register.js
@@ -0,0 +1,62 @@
+import { Link } from "react-router-dom";
+import { auth, db } from "../firebase";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { addDoc, collection, doc } from "firebase/firestore";
+
+const Register = () => {
+
+    const [ user, setUser ] = useState({
+        email: "",
+        password: "",
+    });
+
+    const [ registerDetails, setRegisterDetails ] = useState({
+        loading: false,
+        error: null,
+    })
+
+    const navigate = useNavigate();
+    
+    const createAccount = async (e) => {
+        setRegisterDetails({...registerDetails, error: null});
+        e.preventDefault();
+        
+        setRegisterDetails({...registerDetails, loading: true});
+
+        setUser({email: e.target.email.value, password: e.target.password.value})
+
+        await createUserWithEmailAndPassword(auth, user.email, user.password)
+        .catch(err => setRegisterDetails({...registerDetails, error: err.message}));
+
+        await addDoc(collection(db, "users"), {
+            ...user
+        })
+        navigate("/library");
+    }
+
+    const handleChange = (e) => {
+        e.preventDefault();
+        setUser({...user, [e.target.name]: e.target.value});
+    }
+
+    return (
+        <form onSubmit={createAccount} className="register">
+            <h2>Register account</h2>
+            <section>
+                <label htmlFor="email">Email:</label>
+                <input type="email" name="email" onChange={handleChange} value={user.email}/>
+            </section>
+            <section>
+                <label htmlFor="password">Password:</label>
+                <input type="password" name="password" onChange={handleChange} value={user.password}/>
+            </section>
+            <button disabled={registerDetails.loading}>{registerDetails.loading ? "Loading..." : "Create Account"}</button>
+            <Link to="/login"><p>Already have an account?</p></Link>
+            { registerDetails.error ? <p>{registerDetails.error}</p> : null }
+        </form>
+    )
+}
+
+export default Register;
